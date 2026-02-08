0950f404cf7f050dc51aa17b0a51657624b001fa
Diogo Santos <127253177+diogosantos25426@users.noreply.github.com>
2026-02-08 14:56:34 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -48,7 +48,10 @@ if (process.env.NODE_ENV === 'production') {
 // --------------------------------------------------
 // MIDDLEWARES
 // --------------------------------------------------
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173", // ou 3000
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // --------------------------------------------------
